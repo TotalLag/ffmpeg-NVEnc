9171acdc747126d777d951087b32bdc4980ad994
Chris Phan <TotalLag@users.noreply.github.com>
2023-12-18 01:22:44 -0600

    Update copyfiles.sh

--- copyfiles.sh
+++ copyfiles.sh
@@ -14,12 +14,12 @@ cp /app/workspace/bin/ffmpeg /app/ffmpeg/bin
 cp /app/workspace/bin/ffprobe /app/ffmpeg/bin
 cp /app/workspace/bin/ffplay /app/ffmpeg/bin
 bash /app/ldd.sh /app/workspace/bin/ffmpeg /app/ffmpeg/lib/
-OUTPUT_FNAME="ffmpeg-${FFMPEG_VERSION}-ubuntu${VER}.tar.xz"
+OUTPUT_FNAME="ffmpeg-${FFMPEG_VERSION}-rockylinux${VER}.tar.xz"
 XZ_OPT="-9e -T0" tar cJf "/app/artifacts/${OUTPUT_FNAME}" -C /app/ffmpeg .
 ##nvencc archive 
 mkdir -p /app/nvencc/bin
 mkdir -p /app/nvencc/lib
 cp /app/workspace/bin/nvencc /app/nvencc/bin
 bash /app/ldd.sh /app/workspace/bin/nvencc /app/nvencc/lib
-OUTPUT_FNAME="nvencc-${NVENC_VERSION}-ubuntu${VER}.tar.xz"
+OUTPUT_FNAME="nvencc-${NVENC_VERSION}-rockylinux${VER}.tar.xz"
 XZ_OPT="-9e -T0" tar cJf "/app/artifacts/${OUTPUT_FNAME}" -C /app/nvencc .
